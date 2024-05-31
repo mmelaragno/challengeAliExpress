c6ff9c89961bf8d05bf37f21e140e8d92c84b190
Mariel Melaragno <mela.mariel@gmail.com>
2024-05-31 12:35:17 -0300

    add commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,23 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+let url = "https://www.aliexpress.com";
+
+Cypress.Commands.add('enterAliExpress', () => {
+    cy.clearCookies();
+    Cypress.on('uncaught:exception', (err) => {
+        if (err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
+            return false; // Prevents Cypress from failing the test
+        }
+    });
+    cy.visit(url);
+});
+
+Cypress.Commands.add('acceptPromps', () => {
+    cy.get('body').then((body) => {
+        if (body.find('.next-dialog-close').length > 0) {
+            cy.get('.next-dialog-close').click();
+        }
+    });
+});
