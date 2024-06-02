ce6133fea78bffb71c40f583b0bb437deca4591b
Mariel Melaragno <mela.mariel@gmail.com>
2024-06-02 13:13:41 -0300

    I added reporter in html and videos

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,16 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: true,
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "charts": true,
+    "overwrite": false,
+    "html": true,
+    "json": false,
+    "reportDir": "cypress/report/mochawesome-report"},
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
     chromeWebSecurity: false,
   },
